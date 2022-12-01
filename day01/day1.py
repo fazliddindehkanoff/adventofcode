ea174685a1776b2e36df31c51308d3abc4f8236a
DehqonovFazliddin <fazliddindehqonov092@gmail.com>
2022-12-01 16:50:48 +0500

    first day solutions 'advent of code'

--- day01/day1.py
+++ day01/day1.py
@@ -7,5 +7,9 @@ with open('day1.txt') as f:
             subres = 0
         else:
             subres += int(line)
-
+# first solution
 print(max(lines))
+
+#second solution
+lines.sort(reverse=True)
+print(lines[0])
